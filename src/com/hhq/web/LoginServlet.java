018e70f6943bc66213d2f5e582ed9ce094ebd81d
macpro <macpro@huiqihe-mac.local>
2017-07-31 11:32:31 +0800

    ajax login

--- src/com/hhq/web/LoginServlet.java
+++ src/com/hhq/web/LoginServlet.java
@@ -14,21 +14,22 @@ public class LoginServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         request.setCharacterEncoding("utf-8");
-
+        //设置跨域访问
+        response.setHeader("Access-Control-Allow-Origin","*");
         response.setContentType("text/html;charset=utf-8");
         String username = request.getParameter("username");
 
         PrintWriter out = response.getWriter();
 
-        String loginResult = "登录成功！欢迎您 "+username+"!";
+        String loginResult = "登录成功！欢迎您 "+username+"! -----";
         out.println(loginResult);
+        /*
         Enumeration<String> names = request.getHeaderNames();
-
         while (names.hasMoreElements()){
 
             String name = names.nextElement();
             out.println( name+":"+request.getHeader(name)+"  ");
-        }
+        }*/
 
         out.close();
     }
